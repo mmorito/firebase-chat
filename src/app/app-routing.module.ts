dd3ac5d74fc65928ccc05c03e60b92c791d26431
mmorito <morito@mnes.org>
2020-10-06 19:30:02 +0900

    Add: chat component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ChatComponent } from './chat/chat.component';
+
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: '**', component: ChatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
