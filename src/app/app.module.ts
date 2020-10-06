b177573e893226289de9c4468b598c369a7df741
mmorito <morito@mnes.org>
2020-10-06 19:48:02 +0900

    Add: angular configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChatComponent
-  ],
+  declarations: [AppComponent, ChatComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD5GT0aRAFiaNQZKtYBbzsK9La8xVcu4mI',
+    authDomain: 'mnes-cp100-3.firebaseapp.com',
+    databaseURL: 'https://mnes-cp100-3.firebaseio.com',
+    projectId: 'mnes-cp100-3',
+    storageBucket: 'mnes-cp100-3.appspot.com',
+    messagingSenderId: '257539419600',
+    appId: '1:257539419600:web:d249319bbfb95d70015dd5',
+  },
 };
 
 /*
